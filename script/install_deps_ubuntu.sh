2e83b490a8c20bcfa8635d55c4904017bfaffd51
bvlgah <octopus.busts_0w@icloud.com>
2024-03-31 14:55:08 +0800

    Fix typos of g++

--- script/install_deps_ubuntu.sh
+++ script/install_deps_ubuntu.sh
@@ -1,8 +1,7 @@
 set -ex
 build_deps=('clang' 'lld' 'ninja-build' 'cmake' 'ccache'
-            'gcc-riscv64-linux-gnu' 'gcc++-riscv64-linux-gnu'
-            'tar' 'xz-utils' 'python3' 'python-is-python3' 'python3-pip'
-            'python3-venv')
+            'gcc-riscv64-linux-gnu' 'g++-riscv64-linux-gnu' 'tar' 'xz-utils'
+            'python3' 'python-is-python3' 'python3-pip' 'python3-venv')
 user_id=$(id -u)
 
 if [[ ${user_id} == '0' ]]; then
