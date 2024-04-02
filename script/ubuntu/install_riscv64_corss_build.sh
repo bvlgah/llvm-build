0ba9f3455e420eb3238fbd872b0f6a1fca8c5a7c
bvlgah <octopus.busts_0w@icloud.com>
2024-04-02 16:02:00 +0800

    Use LLVM for cross building

--- script/ubuntu/install_riscv64_corss_build.sh
+++ script/ubuntu/install_riscv64_corss_build.sh
@@ -1,8 +1,7 @@
 set -ex
 
 curr_dir=$(cd $(dirname $0) && pwd)
-build_deps=('ninja-build' 'cmake' 'gcc-riscv64-linux-gnu'
-            'g++-riscv64-linux-gnu' 'tar' 'xz-utils'
+build_deps=('ninja-build' 'cmake' 'tar' 'xz-utils'
             'python3' 'python-is-python3' 'python3-pip' 'python3-venv')
 
 . $curr_dir/apt_install.sh
