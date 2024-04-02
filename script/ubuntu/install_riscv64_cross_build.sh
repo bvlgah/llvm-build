9f3c1e99be25aeb2bd49b7070ee48bee30c6e9ee
bvlgah <octopus.busts_0w@icloud.com>
2024-04-02 17:18:22 +0800

    Add ccache to dependencies for cross building

--- script/ubuntu/install_riscv64_cross_build.sh
+++ script/ubuntu/install_riscv64_cross_build.sh
@@ -1,7 +1,7 @@
 set -ex
 
 curr_dir=$(cd $(dirname $0) && pwd)
-build_deps=('ninja-build' 'cmake' 'tar' 'xz-utils'
+build_deps=('ninja-build' 'cmake' 'tar' 'xz-utils' 'ccache'
             'gcc-riscv64-linux-gnu' 'g++-riscv64-linux-gnu'
             'python3' 'python-is-python3' 'python3-pip' 'python3-venv')
 
