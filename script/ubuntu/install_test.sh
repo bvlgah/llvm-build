b7e648b2904e5802c8125814bd1ef36c6a312bad
bvlgah <octopus.busts_0w@icloud.com>
2024-04-03 09:25:47 +0800

    Modify LLVM Test workflow to install dependencies first

--- script/ubuntu/install_test.sh
+++ script/ubuntu/install_test.sh
@@ -2,7 +2,7 @@ set -ex
 
 curr_dir=$(cd $(dirname $0) && pwd)
 test_deps=('tcl' 'tk-dev' 'python3' 'python-is-python3' 'python3-venv'
-           'python3-pip' 'cmake' 'ninja-build')
+           'python3-pip' 'cmake' 'ninja-build' 'xz-utils')
 
 . $curr_dir/apt_install.sh
 
